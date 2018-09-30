e245536f996d55664219110da083414f61676d5e
DarylAranha <daryl.aranha@gmail.com>
2018-09-30 23:07:48 +0530

    added permutation logic

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 const ud = require('urban-dictionary');
 
-var deff = document.getElementById("word");
+var words = document.getElementById("word");
 var btn = document.getElementById("btn");
 var display = document.getElementById("display");
+var displayWords = [];
 
 btn.addEventListener('click', () => {
-    definition = deff.value;
-
-    ud.term(definition).then((result) => {
-        const entries = result.entries
-        console.log(entries[0].word)
-        display.innerText = entries[0].definition;
-        console.log(entries[0].definition)
-        console.log(entries[0].example)
-    }).catch((error) => {
-        console.error(error.message)
-    })
+    var word = words.value.split(" ");
+    calculatePermutation(word);
 })
 
+function swap(x,y){
+    return [y,x];
+}
 
+function permute(str,l,r){
+    let i;
+    if(l==r) {
+        getDefinition(str.join(''));
+    }
+    else{
+        for(i=l; i<=r; i++){
+            [str[l],str[i]] = swap(str[l],str[i]);
+			permute(str, l+1, r);
+            [str[l], str[i]] = swap(str[l], str[i]);
+        }
+    }
+}
+
+function calculatePermutation(arr) {
+    for(let i = 2; i<=arr.length; i++){
+        let singleCombination = arr.slice(0, i);
+        permute(singleCombination, 0, (singleCombination.length)-1);
+    }
+    console.dir(displayWords);
+}
+
+function getDefinition(definition) {
+    ud.term(definition).then((result) => {
+        const entries = result.entries
+        let wd = entries[0].word;
+        let definition = entries[0].definition;
+        displayWords.push([wd,definition] );
+    }).catch((error) => {
+        // console.error(error.message)
+    })
+}
